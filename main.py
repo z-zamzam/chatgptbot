229ddaa176ae55160c0be5c365a404236eea6db7
z-zamzam <127634089+z-zamzam@users.noreply.github.com>
2023-04-01 22:04:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import openai
 import os
 
-openai.api_key = os.getenv('API_PASSWORD')
+openai.api_key = os.environ['API_KEY']
 
+# Retrieving the prompts and cleaning up the data into a usable format
 with open('prompts.txt', encoding="utf8") as file:
     data = file.read().splitlines()
     data_dict = {x.split(',')[0]: x.split(',')[1] for x in data}
     prompts_dict = {key.replace('"', ''): value.replace('"', '') for (key, value) in data_dict.items()}
 
-
+# Setting the bot's behavior by using an input and selecting the prompt accordingly
 user_choice = input("How would you like the chatbot to act?\n")
+
+# Setting up the loop for chatting with the bot
 while True:
     msg_content = input("User: ")
     chat_response = ''
